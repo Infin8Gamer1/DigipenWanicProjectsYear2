c7bbe2aa02c38973f1d5ced2ab4350317f55c508
Infin8Gamer1 <Jacob@mjhsoftware.com>
2018-11-07 15:05:18 -0800

    added turnin files

---
+++ CS230/Lab5 TurninFiles/Behaviors.h
@@ -0,0 +1,46 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Behaviors.h
+// Author(s):	Jeremy Kings (j.kings)
+// Project:		BetaFramework
+// Course:		WANIC VGP2 2018-2019
+//
+// Copyright © 2018 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+//------------------------------------------------------------------------------
+// Include Files:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Forward References:
+//------------------------------------------------------------------------------
+
+class Transform;
+class Physics;
+class Animation;
+
+//------------------------------------------------------------------------------
+// Public Functions:
+//------------------------------------------------------------------------------
+
+namespace Behaviors
+{
+	// Movement behavior for the triangle/ship.
+	// Params:
+	//	 transform = The transform component of the ship.
+	//   physics = The physics component of the ship.
+	void UpdateShip(Transform* transform, Physics* physics);
+	
+	// Movement behavior for the monkey.
+	// Params:
+	//	 transform = The transform component of the monkey.
+	//   physics = The physics component of the monkey.
+	//   animation = the animation component of the monkey
+	void UpdateMonkey(Transform* transform, Physics* physics, Animation* animation);
+}
