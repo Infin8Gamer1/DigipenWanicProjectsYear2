66e925ecd2069398a19041f48ca555d56d4e520c
Infin8Gamer1 <Jacob@mjhsoftware.com>
2019-03-20 15:10:23 -0700

    started doing a thing

--- Exams/ProgrammingExam1/ProgrammingExam/ProgrammingExam/myArray.cpp
+++ Exams/ProgrammingExam1/ProgrammingExam/ProgrammingExam/myArray.cpp
@@ -16,3 +16,40 @@ int myArray::get_size(void) const
 {
     return size;
 }
+
+void myArray::push(int input)
+{
+}
+
+void myArray::growArray()
+{
+	//save the original capacity
+	unsigned originalCapacity = capacity;
+
+	// Double the capacity
+	capacity = (capacity) ? capacity * 2 : 1;
+
+	//allocate a fresh array
+	int *freshArray = new int[capacity];
+
+	//copy the data over
+	for (unsigned i = 0; i < originalCapacity; i++)
+	{
+		freshArray[i] = numbers[i];
+	}
+
+	//free up the memory and set the array to be the fresh one
+	delete numbers;
+
+	numbers = freshArray;
+}
+
+std::ostream & operator<<(std::ostream & output, const myArray & m_Array)
+{
+	// TODO: insert return statement here
+	for (int i = 0; i < m_Array.size; i++)
+	{
+		output << m_Array.numbers[i] << ", ";
+	}
+	return output;
+}
--- Exams/ProgrammingExam1/ProgrammingExam/ProgrammingExam/myArray.h
+++ Exams/ProgrammingExam1/ProgrammingExam/ProgrammingExam/myArray.h
@@ -9,7 +9,13 @@ public:
     int get_capacity(void) const;
     int get_size(void) const;
 
+	void push(int input);
+
+	friend std::ostream &operator<<(std::ostream &output, const myArray &m_Array);
+
 private:
+	void growArray();
+
     int *numbers;
     int size;
     int capacity;
