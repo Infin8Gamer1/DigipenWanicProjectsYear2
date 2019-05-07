2029fd3e3ea00dda0c2743474b81ce5fab6f3aa2
Infin8Gamer1 <Jacob@mjhsoftware.com>
2019-05-07 14:15:53 -0700

    Please Make it stop... THE PAIN, THE PAIN

---
+++ Learn OpenGL/Lab 1/LearnOpenGL_Student/src/Main.cpp
@@ -0,0 +1,189 @@
+#include <glad.h>  // Include OpenGL functions
+#include <glfw3.h> // Include GLFW functions
+#include <iostream>
+
+#pragma comment(lib, "glfw3.lib") // Link to GLFW lib
+
+#define WindowWidth 800
+#define WindowHeight 600
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// Main entry point for application
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// create a new window
+	GLFWwindow* window = glfwCreateWindow(WindowWidth, WindowHeight, "LearnOpenGL", NULL, NULL);
+	//if it didn't create then just give up and die
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	//tell openGL the window size
+	glViewport(0, 0, WindowWidth, WindowHeight);
+
+	//set the window resize call back
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//set background color
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//compile shaders
+#pragma region VertexShader
+
+	const char *vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+#pragma endregion
+
+#pragma region FragmentShader
+
+	const char *fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	int  success2;
+	char infoLog2[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success2);
+
+	if (!success2)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog2);
+		std::cout << "ERROR::SHADER::Fragment::COMPILATION_FAILED\n" << infoLog2 << std::endl;
+	}
+
+#pragma endregion
+
+	//create shader progam and use it
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER_PROGRAM::FAILED\n" << infoLog << std::endl;
+	}
+
+	glUseProgram(shaderProgram);
+	
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//CREATE BUFFERS
+	//CREATE VAO
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	//Create triangle
+	float vertices[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// 3. then set our vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//input
+		processInput(window);
+
+		//rendering commands
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
+
+		//check and call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	glfwTerminate();
+	return 0;
+}
---
+++ Learn OpenGL/Lab 1/LearnOpenGL_Student/src/glm_include.h
@@ -0,0 +1,32 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	glm_include.h
+// Author(s):	Jeremy Kings
+// Project:		Beta Engine
+// Course:		WANIC VGP2
+//
+// Copyright © 2018 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+#pragma once
+
+#pragma warning(push)
+
+// Non-standard extension
+#pragma warning(disable : 4201)
+// Conditional expression is constant
+#pragma warning(disable : 4127)
+
+//------------------------------------------------------------------------------
+// Includes:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#pragma warning(pop)
+
+//------------------------------------------------------------------------------
